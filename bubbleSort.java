fb497eee1a0a339dea9d232ce3dcbf54b88affe9
Dwarakeshwaran <dwaki98@gmail.com>
2024-11-27 16:22:19 -0500

    Update bubbleSort.java

--- bubbleSort.java
+++ bubbleSort.java
@@ -8,11 +8,13 @@ public class main
        int[] arr = { 6, 5, 4, 3, 2, 1};
 
        /*
-            The Idea of Selection Sort is to take the MINIMUM Values by using the inner loop pointer and group them in the beginning of the index.
+            The Idea of Bubble Sort is to take the MINIMUM Values by using the inner loop pointer and group them (or bubble them) in the beginning/end of the index.
             So, what will happen to the already existing beginning index? we can secure them by moving them to the index where the minimum value was present earlier.
             The outer loop pointer will be incremented so that the sorted beginning indices won't get disturbed. (int j = i)
             At the end of 1st iteration of the inner loop, the minimum values will be available in the beginning of the index. 
             And the remaining order will be shifted to the next consecutive indices.
+
+            THIS IS THE WORST TYPE OF SORT. EVEN OBAMA KNOWS IT.
        */
 
        //654321 - inner loop itr 1
